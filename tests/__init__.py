3e62579b736c0e3867062170147d888873dd6df6
Rene Peinthor <rene.peinthor@linbit.com>
2018-02-12 09:39:14 +0100

    rework test organization

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,35 @@
-from .test_list_commands import *
-from .test_nodes import *
-from .test_usecases import *
-from .test_utils import *
-from .test_props import *
+import unittest
+from . import test_utils
+from . import test_ctrl_list_commands, test_ctrl_nodes, test_ctrl_usecases, test_utils, test_ctrl_props
+
+_controller_tests = [
+    "tests.test_ctrl_list_commands",
+    "tests.test_ctrl_nodes",
+    "tests.test_ctrl_usecases",
+    "tests.test_ctrl_props"
+]
+
+_std_tests = [
+    "tests.test_utils"
+]
 
 
-"""
 def load_tests(loader, tests, pattern):
-    suite = TestSuite()
-    tests = loader.loadTestsFromTestCase(TestListCommands)
-    suite.addTest(tests)
+    suite = unittest.TestSuite()
+    loaded_tests = loader.loadTestsFromNames(_controller_tests + _std_tests)
+    suite.addTest(loaded_tests)
     return suite
-"""
+
+
+def load_all():
+    suite = unittest.TestSuite()
+    loaded_tests = unittest.defaultTestLoader.loadTestsFromNames(_controller_tests + _std_tests)
+    suite.addTest(loaded_tests)
+    return suite
+
+
+def test_without_controller():
+    suite = unittest.TestSuite()
+    loaded_tests = unittest.defaultTestLoader.loadTestsFromNames(_std_tests)
+    suite.addTest(loaded_tests)
+    return suite
