cb74f423c44bbeeed198f788a1374ed0b792b84f
Joel Colledge <joel.colledge@linbit.com>
2018-04-24 09:34:43 +0200

    Add create-watch to the hidden commands list to fix unit test

--- linstor/commands/commands.py
+++ linstor/commands/commands.py
@@ -127,7 +127,8 @@ class Commands(object):
     Hidden = [
         EXIT,
         DMMIGRATE,
-        GEN_ZSH_COMPLETER
+        GEN_ZSH_COMPLETER,
+        CREATE_WATCH
     ]
 
     def __init__(self):
