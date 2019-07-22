f82d6d482a08ee689e9c37542148f45839aa81c2
Rene Peinthor <rene.peinthor@linbit.com>
2019-07-22 13:22:15 +0200

    add allow-insecure-auth to global args filter

--- linstor_client/utils.py
+++ linstor_client/utils.py
@@ -181,7 +181,7 @@ def filter_new_args(unsetprefix, args):
     reserved_keys = [
         "func", "optsobj", "common", "command",
         "controllers", "warn_as_error", "no_utf8", "no_color",
-        "machine_readable", "disable_config", "timeout", "verbose", "output_version", "curl"
+        "machine_readable", "disable_config", "timeout", "verbose", "output_version", "curl", "allow_insecure_auth"
     ]
     for k, v in args.__dict__.items():
         if v is not None and k not in reserved_keys:
