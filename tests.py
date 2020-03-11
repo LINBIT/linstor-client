5a5ea8ca38c8f7e25c52e6b40f6d5509776221df
Rene Peinthor <rene.peinthor@linbit.com>
2020-03-11 13:27:13 +0100

    tests: fix suite not returning correct exit code

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
 import unittest
+import sys
 
 if __name__ == '__main__':
     import xmlrunner
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir='tests')
     runner = xmlrunner.XMLTestRunner(output='test-reports')
-    runner.run(suite)
+    res = runner.run(suite)
+    sys.exit(1 if len(res.errors) > 0 else 0)
