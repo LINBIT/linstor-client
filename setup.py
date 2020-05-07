56b4f0bca54c5f4f672cb11b5561c05b83d39fae
Rene Peinthor <rene.peinthor@linbit.com>
2020-05-07 08:16:34 +0000

    prepare release v1.1.0

--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ setup(
         "linstor_client.commands"
     ],
     install_requires=[
-        "python-linstor>=1.0.15"
+        "python-linstor>=1.1.0"
     ],
     py_modules=["linstor_client_main"],
     scripts=["scripts/linstor"],
