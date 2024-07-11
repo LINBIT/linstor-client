e16eabb83fec369488efe84d1054b95a1aa66e9d
Rene Peinthor <rene.peinthor@linbit.com>
2024-07-11 09:28:32 +0000

    prepare release v1.23.0

--- linstor_client/consts.py
+++ linstor_client/consts.py
@@ -21,7 +21,7 @@
 Global constants for linstor client
 """
 
-VERSION = "1.22.1"
+VERSION = "1.23.0"
 
 try:
     from linstor_client.consts_githash import GITHASH
--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ setup(
         "linstor_client.commands"
     ],
     install_requires=[
-        "python-linstor>=1.22.0"
+        "python-linstor>=1.23.0"
     ],
     py_modules=["linstor_client_main"],
     scripts=["scripts/linstor"],
